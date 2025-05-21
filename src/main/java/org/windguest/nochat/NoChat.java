59d78af89a8dbc0c2d20a6854a68a2e3f03f7c35
FengKeWG <bowen13861365586@gmail.com>
2025-05-21 22:00:53 +0800

    Initial commit

---
+++ src/main/java/org/windguest/nochat/NoChat.java
@@ -0,0 +1,19 @@
+package org.windguest.nochat;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NoChat extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
+        event.setCancelled(true);
+    }
+}
